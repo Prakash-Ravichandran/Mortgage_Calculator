455916ae0d083aeefd1e1e2003a642ef19d7e5f7
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-04-11 20:30:05 +0530

    build: + state injection - App.js => SliderComponent function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import { Container } from "@mui/system";
 import { Grid } from "@mui/material";
@@ -8,13 +8,21 @@ import TenureSelect from "./Components/TenureSelect";
 import Result from "./Components/Result";
 
 function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  });
+
   return (
     <React.Fragment>
       <div className="App">
         <Navbar />
         <Container maxWidth="x1" sx={{ marginTop: 4 }}>
           <Grid container spacing={5} alignItems="center">
-            <SliderSelect />
+            <SliderSelect data={data} setData={setData} />
             <TenureSelect />
           </Grid>
           <Grid item xs={12} md={6}>
