ef5e289b06cad5178b5d4fe3224eb447df9fce54
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-04-12 13:02:50 +0530

    Build: tenure component added

--- src/Components/TenureSelect.js
+++ src/Components/TenureSelect.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { InputLabel } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import { FormControl } from "@mui/material";
+import { Select } from "@mui/material";
 
-export default function TenureSelect() {
+export default function TenureSelect({ data, setData }) {
+  const handleChange = (event, value) => {
+    setData({ ...data, loanTerm: value });
+  };
+
   return (
     <>
-      <h5>Tenure Select Component</h5>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Tenure</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={data.loanTerm}
+          label="Tenure"
+          defaultValue={5}
+          onChange={handleChange}
+        >
+          <MenuItem value={5}>5 years</MenuItem>
+          <MenuItem value={10}>10 years</MenuItem>
+          <MenuItem value={15}>15 years</MenuItem>
+          <MenuItem value={20}>20 years</MenuItem>
+          <MenuItem value={25}>25 years</MenuItem>
+        </Select>
+      </FormControl>
     </>
   );
 }
